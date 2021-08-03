586204d703700215b134c622a273f53d469573b7
Serhii Tkachenko <x.ray868@gmail.com>
2021-08-03 13:31:59 +0300

    update

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ const colors = [
   "#795548",
 ];
 
+const refs = {
+  startBtn: document.querySelector("button[data-action='start']"),
+  stopBtn: document.querySelector("button[data-action='stop']"),
+};
+
+refs.startBtn.addEventListener("click", () => {
+  timer.startColorSet();
+});
+refs.stopBtn.addEventListener("click", () => {
+  timer.stopColorSet();
+});
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const timer = {
+  intervalId: null,
+  isActive: false,
+
+  startColorSet() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const indexColor = randomIntegerFromInterval(0, colors.length - 1);
+      document.body.style.backgroundColor = colors[indexColor];
+    }, 1000);
+  },
+
+  stopColorSet() {
+    this.isActive = false;
+    clearInterval(this.intervalId);
+  },
 };
